36e4defc2ea713cbd26697cb311a9efd2fd0e891
Anna Tanisheva <anna-tanisheva@yandex.ru>
2022-11-07 16:31:47 +0300

    feat: add resolution for task 2

--- src/app/tasks/task2/task2.component.ts
+++ src/app/tasks/task2/task2.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, Subscription, tap } from 'rxjs';
+import { Task2Service } from './task2.service';
 
 @Component({
   selector: 'app-task2',
   templateUrl: './task2.component.html',
-  styleUrls: ['./task2.component.scss']
+  styleUrls: ['./task2.component.scss'],
 })
 export class Task2Component implements OnInit {
-
-  constructor() { }
+  constructor(private task2Service: Task2Service) {}
+  randomNumbers$$: Observable<number[]> = this.task2Service.randomNumbers$$;
+  randomNumbersSub: Subscription | undefined;
+  stats$$ = new BehaviorSubject<{
+    count: number | null;
+    min: number | null;
+    max: number | null;
+    average: number | null;
+  }>({ count: null, min: null, max: null, average: null });
 
   ngOnInit(): void {
+    this.randomNumbersSub = this.randomNumbers$$
+      .pipe(
+        tap((randomNumbers) => {
+          this.stats$$.next({
+            count: randomNumbers.length,
+            min:
+              randomNumbers.length > 0
+                ? Math.min.apply(null, randomNumbers)
+                : 0,
+            max:
+              randomNumbers.length > 0
+                ? Math.max.apply(null, randomNumbers)
+                : 0,
+            average:
+              randomNumbers.length > 0
+                ? randomNumbers.reduce((a, b) => {
+                    return a + b;
+                  }, 0) / randomNumbers.length
+                : 0,
+          });
+        })
+      )
+      .subscribe();
   }
 
+  onAddNumber() {
+    this.task2Service.addRandomNumber();
+  }
 }
--- src/app/tasks/task2/task2.service.ts
+++ src/app/tasks/task2/task2.service.ts
@@ -1,27 +1,31 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 
 export interface IStore {
-  randomNumbers: number[]
+  randomNumbers: number[];
 }
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class Task2Service {
-
   private _store: BehaviorSubject<IStore> = new BehaviorSubject<IStore>({
-    randomNumbers: []
+    randomNumbers: [],
   });
 
-  // public addRandomNumber(): void {
-  //
-  // }
-  //
-  // private _getRandomNumber(max: number = 100): number {
-  //
-  // }
-  //
-  // private _updateStore(data: Partial<IStore>): void {
-  //
-  // }
+  public randomNumbers$$: Observable<number[]> = this._store.pipe(
+    map((store) => store.randomNumbers)
+  );
 
+  public addRandomNumber(): void {
+    const randomNumbersArr = this._store.getValue().randomNumbers;
+    randomNumbersArr.push(this._getRandomNumber());
+    this._updateStore({ randomNumbers: randomNumbersArr });
+  }
+
+  private _getRandomNumber(max: number = 100): number {
+    return Math.floor(Math.random() * max);
+  }
+
+  private _updateStore(data: Partial<IStore>): void {
+    this._store.next({ ...this._store.getValue(), ...data });
+  }
 }
